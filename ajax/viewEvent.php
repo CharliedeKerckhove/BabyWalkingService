9d6891ba28bb163a3943e85e20c2ffb9fb8f3f13
CrazyC008 <DEKC1_16@uni.worc.ac.uk>
2019-09-28 23:01:04 +0100

    creation of viewEvent

---
+++ ajax/viewEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+$data = array();
+
+/*link to database*/
+require_once('../includes/db.php');
+
+/*select events*/
+$query1 ='  SELECT BookingDate, StartTime, EndTime, CollectionAddress, CollectionPostcode, ServiceName, `child`.FirstName, PhoneNumber 
+            FROM `booking` 
+            INNER JOIN `service`
+            ON `booking`.ServiceID = `service`.ServiceID
+            INNER JOIN `child`
+            ON `booking`.ChildID = `child`.ChildID
+            INNER JOIN `carer`
+            ON `child`.CarerID = `carer`.CarerID
+            WHERE `booking`.BookingID = :BookingID
+        ';
+$smt1 = $DBH->prepare($query1);
+$smt1->execute(
+    array(':BookingID' => $_POST['BookingID'])
+);
+
+$result = $smt1->fetch(PDO::FETCH_ASSOC);
+$data[] = array(
+    'BookingDate' => $result['BookingDate'],
+    'StartTime' => $result['StartTime'],
+    'EndTime' => $result['EndTime'],
+    'CollectionAddress' => $result['CollectionAddress'],
+    'CollectionPostcode' => $result['CollectionPostcode'],
+    'ServiceName' => $result['ServiceName'],
+    'FirstName' => $result['FirstName'],
+    'PhoneNumber' => $result['PhoneNumber']
+);
+return json_encode($data);
+
+?>
